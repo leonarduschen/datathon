88cb6092dbfa6062cf5ba8980ad4ef1250a0f783
dandykennedy@gmail.com <dandykennedy@gmail.com>
2020-07-12 17:00:19 +0800

    'fix'

--- TestModel.py
+++ TestModel.py
@@ -9,5 +9,5 @@ Created on Sun Jul 12 14:13:26 2020
 def testModelLoss(model,data,criterion):
     inputs,labels = data['test']
     preds = model(inputs.float())
-    loss = loss_fn(preds,labels).item()
+    loss = criterion(preds,labels).item()
     return loss
