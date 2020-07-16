5e30a2ec87c73d356840dac3f4c08f77d60e3de4
Nicholas ST <nichostst@pop-os.localdomain>
2020-07-16 18:40:30 +0800

    Dictionarize arguments

--- main.py
+++ main.py
@@ -15,6 +15,32 @@ from test_model import test_model_loss
 from baseline_model import baseline_model_loss
 from preprocess import Dataset
 
+
+cols = ['speed-guitrancourt', 'speed-lieusaint', 'speed-lvs-pussay',
+        'speed-parc-du-gatinais', 'speed-arville', 'speed-boissy-la-riviere',
+        'speed-angerville-1', 'speed-angerville-2', 'speed-guitrancourt-b',
+        'speed-lieusaint-b', 'speed-lvs-pussay-b', 'speed-parc-du-gatinais-b',
+        'speed-arville-b', 'speed-boissy-la-riviere-b', 'speed-angerville-1-b',
+        'speed-angerville-2-b', 'Energy']
+
+feature_kwargs = {'lags_period': [0, 1, 2],
+                  'lags_columns': cols,
+                  'SMA_windows': [3, 12, 48],
+                  'SMA_columns': cols,
+                  'SMSTD_windows': [6, 24, 72],
+                  'SMSTD_columns': cols}
+
+split_kwargs = {'train_pctg': 0.8,
+                'val_pctg': 0.1,
+                'test_pctg': 0.1}
+
+constructor = (
+    Layer('Linear', None, 128, 'ReLU'),
+    Layer('Linear', 128, 64, 'ReLU'),
+    Layer('Linear', 64, 32, 'ReLU'),
+    Layer('Linear', 32, 1, None)
+)
+
 if __name__ == '__main__':
     if torch.cuda.is_available():
         # you can continue going on here, like cuda:1 cuda:2....etc.
@@ -27,25 +53,12 @@ if __name__ == '__main__':
     # Load
     df = pd.read_csv('./rawdata/consolidated_autocaffe_data.csv')
 
-    cols = [
-        'speed-guitrancourt', 'speed-lieusaint', 'speed-lvs-pussay',
-        'speed-parc-du-gatinais', 'speed-arville', 'speed-boissy-la-riviere',
-        'speed-angerville-1', 'speed-angerville-2', 'speed-guitrancourt-b',
-        'speed-lieusaint-b', 'speed-lvs-pussay-b', 'speed-parc-du-gatinais-b',
-        'speed-arville-b', 'speed-boissy-la-riviere-b', 'speed-angerville-1-b',
-        'speed-angerville-2-b'
-        ]
     # Generate Features
-    dataset = Dataset(
-        df,
-        lags_period=[1, 2, 3], lags_columns=cols,
-        SMA_windows=[3, 12, 48], SMA_columns=cols,
-        SMSTD_windows=[6, 24, 72], SMSTD_columns=cols
-        )
+    dataset = Dataset(df, **feature_kwargs)
 
     # Aggregate all features, split, clean
     dataset.generate_final_dataset()
-    dataset.train_val_test_split(dataset.final_df, 0.8, 0.1, 0.1)
+    dataset.train_val_test_split(dataset.final_df, **split_kwargs)
     dataset.clean_train_val_test()
     dataset.scale_train_val_test(scaler=StandardScaler())
     print(dataset.train.columns)
@@ -56,13 +69,6 @@ if __name__ == '__main__':
 
     # GENERATE NETWORK
     features = data['train'][0].shape[1]
-
-    constructor = (
-        Layer('Linear', None, 128, 'ReLU'),
-        Layer('Linear', 128, 64, 'ReLU'),
-        Layer('Linear', 64, 32, 'ReLU'),
-        Layer('Linear', 32, 1, None)
-    )
 
     network = generateANN(constructor=constructor,
                           input_shape=features).to(device)
